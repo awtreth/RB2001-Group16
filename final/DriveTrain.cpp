79d6f76b8662d84574c7913caac6470ac523ecb7
Mateus Amarante Araujo <mateus.amarujo@gmail.com>
2015-10-12 19:06:41 -0400

    Commented DriveTrain and corrected if statement

--- final/DriveTrain.cpp
+++ final/DriveTrain.cpp
@@ -37,8 +37,6 @@ void DriveTrain::stop()
 
 void DriveTrain::drive(int left_value, int right_value)//-90~90
 {
-  //	right_value += COMPENSATION/2;
-  //	left_value  -= COMPENSATION/2;
 
   left_value = constrain(left_value, -90, 90);
   right_value = constrain(right_value, -90, 90);
@@ -83,41 +81,43 @@ void DriveTrain::turn(TurnDirection dir, int speed)//-90~90
 }
 
 
-//SMART  MOVMENTS
-/*void DriveTrain::resume()
-{
-	//TODO
-}*/
+//SMART MOVEMENTS
 
 int DriveTrain::turn90(TurnDirection dir)
 {
+    //Static variable are initialized just one time, at the first call
   static bool came_from_white = false;
   static bool already_turn = false;
+  
   long int last_time;
   
   LineSensorIndex turn_sensor;
 
   if (dir == RIGHT) {
-    turn_sensor = BACK_LS;
+    turn_sensor = BACK_LS; //TODO: put turn_sensor as an argument
   } else if (dir == LEFT) {
     turn_sensor = SIDE_LS;
   }
   
+  //This condition avoid not necessary turn commands
   if (!already_turn)
-  {
+  {//Since this function is supposed to be called multiple times (in a loop). It just need to send the turn command one time
     last_time = millis();
     this->turn(dir);
     already_turn = true;
   }
 
-  if (!came_from_white || (millis()-last_time > 500))
+    //this waiting was necessary, because sometimes the sensor starts facing the white surface in the wrong side of the line (opposite to the turn direction). In this case, the first line crossing the robot senses is the wrong line (where it was suppose to be facing at the beggining). So, the sulution is to start turning for a while and then verify if it's black (white first)
+  if (!came_from_white && (millis()-last_time > 500))
   {
+    //It waits the turn_sensor to sense white to start looking for black state (when it hits a crossing line)
+    //it is necessary, because the sensor can start facing a black line
     if (ln_sensor[turn_sensor].isWhite())
       came_from_white = true;
   }
 
   if (came_from_white)
-  {
+  {//It just start checking if the sensor is black when it has already sensed white once
     if (ln_sensor[turn_sensor].isBlack())
     {
       this->stop();
@@ -140,14 +140,17 @@ int DriveTrain::turn90Right()
   return turn90(RIGHT);
 }
 
+//this function use similiar techinics as the used in DriveTrain::turn90() function
 int DriveTrain::moveStraight(int n_line_crossings, int speed)
 {
+    //Static variable are initialized just one time, at the first call
   static bool new_move = true;
   static int missing_lines = n_line_crossings;
   static bool came_from_white = false;
 
+    //This condition control polling
   if (new_move)
-  {
+  {//Since this function is supposed to be called multiple times (in a loop). It has to reset the line counting at the first call
     if (n_line_crossings == 0) n_line_crossings = -1;
     missing_lines = n_line_crossings;
     new_move = false;
@@ -175,6 +178,7 @@ int DriveTrain::moveStraight(int n_line_crossings, int speed)
 
   int value = pid.calc(ln_sensor[RIGHT_LS].read() - ln_sensor[LEFT_LS].read());
 
+    //accelerate one and desaccelerate the other so that it turns
   this->drive(speed + value, speed - value);
 
   //FIXME: the next 2 if statements are repeated in turn90. Maybe we can create a method for this
@@ -192,8 +196,6 @@ int DriveTrain::moveStraight(int n_line_crossings, int speed)
       missing_lines--;
     }
   }
-
-  //Serial.println(missing_lines);
 
   if (missing_lines == 0  || ((wall_stopper.isPressed() || reactor_stopper.isPressed()) && (speed > 0)))
   {
--- final/DriveTrain.h
+++ final/DriveTrain.h
@@ -6,69 +6,178 @@
 #include "Switch.h"
 #include "PidController.h"
 
-#include <Servo.h> //it has to be included in the main .ino file
+#include <Servo.h> //it has to be included in the main .ino file too
 
-#define DEFAULT_SPEED 		45
+#define DEFAULT_SPEED 		45	///< 0-90
+
+/**
+ * Right sensor usually turns faster than the left one.
+ * This factor is proportional to the level of compensation (how much faster
+ * to turn the left motor when we want to turn both equally)
+ */
 #define MOTOR_COMPENSATION 	.12
 
+///To index the LineSensor array in DriveTrain
 enum LineSensorIndex{LEFT_LS, RIGHT_LS, SIDE_LS, BACK_LS, N_LINE_SENSORS};
+
+///Arguments for DriveTrain::turn(TurnDirection direction)
 enum TurnDirection{LEFT, RIGHT, TURN_180};
 
+/**
+ * This class provide functions to interact more easily with the drive_train
+ * 
+ * It is designed specifically for RBE 2001 final project, alghough some functions
+ * are quite generic.
+ * 
+ * It is considered that the driveTrain has:
+	 * 2 ServoMotors: skid steer configuration. The servos have to be placed in symetric positions. So, sendind the same input signal for both motors, the motors will turn in opposite directions (one will try to move the drive train forward and the other will try to move the drive train backward)
+	 * 
+	 * 4 LineSensors: 2 for line tracking in the middle, 1 in the back and other in one side of the robot aligned with the centered line tracking sensors;
+	 * 
+	 * 2 Stopper Switches: 1 to sense when the robot hits the reactor and other to sense when it hits the wall of supply or storage areas
+ * 
+ */ 
 class DriveTrain
 {
 	
 	public:
 	
-	DriveTrain(){}//may can set default values for pins
+	DriveTrain(){}//Not used
 	
-	//This constructor didn't work at global scope (I don't know why)
-	DriveTrain(int left_motor_pin, int right_motor_pin);
+	DriveTrain(int left_motor_pin, int right_motor_pin);///< Set the pins of the motors
 	
+	//Attach Functions
 	void attachMotors(int left_motor_pin, int right_motor_pin);
-	void attachLnSensors(int left_pin, int right_pin, int side_pin, int back_pin = 0);//at first we'll try without back_pin
-	void attachStoppers(int reactor_stopper_pin, int wall_stopper_pin);//at first we'll try without back_pin //TODO
+	void attachLnSensors(int left_pin, int right_pin, int side_pin = 0, int back_pin = 0);
+	void attachStoppers(int reactor_stopper_pin, int wall_stopper_pin);
 	
+	
+	/**GENERIC FUNCTIONS
+	 * 
+	 * Without feedback of LineSensors neither Switches
+	*/
+	
+	/** Drive the motors separatedly
+	 * 
+	 * \param left_value signal sent to right Servo (Arduino Library). It is proportional to the torque and the constraint range is -90~90. Positive values make it turns in the direction that makes the drive train move forward 
+	 * \param right_value signal sent to right Servo (Arduino Library). It is proportional to the torque and the constraint range is -90~90. Positive values make it turns in the direction that makes the drive train move forward 
+	 */
 	void drive(int left_value, int right_value);
 
+	///Both motors stop
 	void stop();
   
+	/**Send same value to the motors. Move forward without LineSensors feedback
+	 * 
+	 * \param speed Range:0~90
+	 * @see drive()
+	 */
 	void driveEqual(int speed = DEFAULT_SPEED);
 	
+	/**The robot turns around itself 
+	 * 
+	 * It sends opositte values for drive() function
+	 * 
+	 * \param speed Range:-90~90. Positive - Turn Right; Negative - Turn Left;
+	 * @see drive()
+	 */
 	void turn(int speed);
+	
+	/**The robot turns around itself given the Turn Direction and speed 
+	 * 
+	 * \param dir RIGHT or LEFT
+	 * \param speed Range:0~90
+	 * @see drive()
+	 */
 	void turn(TurnDirection dir, int speed = DEFAULT_SPEED*2/3);
+	//Especific versions of turn(TurnDirection, int)
 	void turnLeft(int speed = DEFAULT_SPEED*2/3);
 	void turnRight(int speed = DEFAULT_SPEED*2/3);
-
-  
-	//SMART MOVMENTS
 	
-  PidController pid;
-  
-	//void resume();
 	
-	//the next functions return DONE(=1) or NOT_DONE_YET(=0)
-	//constants defined in "util.h"
+	/**GENERIC FUNCTIONS
+	 * 
+	 * With feedback of LineSensors neither Switches
+	 * 
+	 * All these functions return DONE (1) or NOT_DONE_YET (0)
+	 * 
+	 * @see util.h
+	 * 
+	 * It is designed to use State Machines
+	 * 
+	 * Actually, these functions would fit better in RobotController class. We prefered to keep them here to save code there
+	 *
+	 * @see RobotController
+	*/
+	
+	
+	/**PidController for for Line Tracking
+	 * 
+	 * Performs pid control for line tracking task
+	 * 
+	 * For now it is in public scope. The user has to set it before using the methods:
+	 * int DriveTrain::moveStraight(int,int);
+	 * int DriveTrain::moveForward(int, int);
+	 * intDriveTrain::moveBackward(int,int);
+	 * 
+	 * @see PidController 
+	 * @see moveStraight()
+	 * @see moveForward()
+	 * @see moveBackward()
+	 */
+	 PidController pid;
+	
+	/** Turn until an specified LineSensor (back or side) detects a line crossing
+	 * 
+	 * Turns in default speed
+	 * 
+	 * \param dir RIGHT or LEFT
+	 * 
+	 * @TODO: we can create a parameter which is the stopper LineSensorIndex
+	 */
 	int turn90(TurnDirection dir);
 	int turn90Left();
 	int turn90Right();
 	
+	/** MoveForward performing LineTracking
+	 * 
+	 * The robot stops when it pass through n_line_crossings or when it hits a wall or a reactor
+	 * 
+	 * \param n_line_crossings <=0 means that the robot will not count line_crossings
+	 * \param speed 0~90
+	 * 
+	 * \TODO Actually, it is a suggestion: n_line_crossings == 0, the robot does nothings
+	 */
 	int moveForward(int n_line_crossings = 0, int speed = DEFAULT_SPEED);
+	/**Same as moveForward(), but backwards
+	 * @see moveForward()
+	*/
 	int moveBackward(int n_line_crossings = 0, int speed = DEFAULT_SPEED);
-	int moveStraight(int n_line_crossings = 0, int speed = DEFAULT_SPEED);//negative speed moves backward
+	/** Generic Function for LineTracking
+	 * 
+	 * \param n_line_crossings
+	 * \param speed -90~90. Positive: Forward; Negative: Backward
+	 * 
+	 * @see moveBackward()
+	 * @see moveForward()
+	 */ 
+	int moveStraight(int n_line_crossings = 0, int speed = DEFAULT_SPEED);
 
-  LineSensor ln_sensor[N_LINE_SENSORS];
+
+	/**Indexed by LineSensorIndex
+	 * 
+	 * @see LineSensor Class
+	 */
+	LineSensor ln_sensor[N_LINE_SENSORS];
+	
 	private:
 	
 	Servo left_motor;
 	Servo right_motor;
 	
-	
-	
 	Switch reactor_stopper;
-  Switch wall_stopper;
+	Switch wall_stopper;
 
-	bool stop_flag;
-	//I think we'll need more flags
 };
 
 #endif
